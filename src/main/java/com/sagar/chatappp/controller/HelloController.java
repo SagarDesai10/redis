1407ede191bb16422944c218b76dd02d8edd6d86
SagarDesai10 <desaisagar8090@gmail.com>
2025-01-18 10:27:18 +0530

    add hello controller

--- src/main/java/com/sagar/chatappp/controller/HelloController.java
+++ src/main/java/com/sagar/chatappp/controller/HelloController.java
@@ -1,10 +1,19 @@
 package com.sagar.chatappp.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/app/v1/chatapp")
 public class HelloController {
+	
+	@GetMapping(value="/hello")
+	public ResponseEntity hello()
+	{
+		return ResponseEntity.ok().body("HELLO");
+	}
 
 }
