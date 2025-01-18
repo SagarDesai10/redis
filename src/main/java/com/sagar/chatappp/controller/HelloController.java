99f9dbbf8160872aed311d7a9ac5f166ca87c312
SagarDesai10 <desaisagar8090@gmail.com>
2025-01-18 11:37:17 +0530

    add thymleaf template

--- src/main/java/com/sagar/chatappp/controller/HelloController.java
+++ src/main/java/com/sagar/chatappp/controller/HelloController.java
@@ -1,19 +1,25 @@
 package com.sagar.chatappp.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/app/v1/chatapp")
 public class HelloController {
 	
 	@GetMapping(value="/hello")
-	public ResponseEntity hello()
+	public ModelAndView hello(ModelMap model)
 	{
-		return ResponseEntity.ok().body("HELLO");
+		model.addAttribute("userMessages","user");
+		model.addAttribute("friendMessages", "friend");
+		model.addAttribute("message","some message");
+		return new ModelAndView("chat",model);
 	}
 
 }
